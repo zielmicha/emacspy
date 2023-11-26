34e4beb8b0643fd810013339fd69e4d71a960386
Carl Williams <chw@milcon.com>
2023-11-26 07:01:00 -0800

    cleaning up and syncing with MacOS

--- stub.c
+++ stub.c
@@ -8,12 +8,6 @@ void PyInit_emacspy(void);
 extern int plugin_is_GPL_compatible;
 
 int emacs_module_init(void* runtime) {
-    static char* lib = "libpython3.11.so.1.0";
-    void *ptr = dlopen(lib, RTLD_LAZY | RTLD_GLOBAL);
-    if ( ptr == NULL ) {
-        fprintf(stderr, "Can't open library: %s\n",dlerror());
-        exit(0);
-    }
     Py_Initialize();
     PyInit_emacspy();
     int result = emacs_module_init_py(runtime);
